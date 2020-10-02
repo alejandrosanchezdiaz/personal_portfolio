b5e417108bdd917dc62158b37c2ae3f3f9572dad
Alex <alesandi.uk@gmail.com>
2020-10-02 11:51:42 +0100

    V4.2
    Final

--- projects/snake_game_JS/JS/game.js
+++ projects/snake_game_JS/JS/game.js
@@ -12,7 +12,7 @@ const gameBoard = document.getElementById('game-board');
 function main(currentTime){
     if (gameOver) {
         if (confirm("You lost. Press OK to restart.")) {
-            window.location = '/'
+            window.location.reload()
         }
         return;
     }
